699c9499b18ab2af548f38ace75fe7e2ca02f316
casaer01 <ecasas182015@gmail.com>
2024-07-02 18:12:52 -0500

    adjusting position of navtop

--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -6,10 +6,8 @@ import './topnavbar.css';
 class Topnavbar extends Component {
     render() {
         return (
-            <header>
-            {/* <!-- Navbar --> */}
-            <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
-                <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-1">
+                <div class="container navBody">
                     <img src={logo} alt="Bakery Logo" class="bakeryLogo" />
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +18,7 @@ class Topnavbar extends Component {
 
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="#!">Offer</a>
+                                <a class="nav-link" href="#!">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#!">Features</a>
@@ -43,15 +41,11 @@ class Topnavbar extends Component {
                         </ul>
 
                     </div>
+
+                    
                     
                 </div>
             </nav>
-            {/* <!-- Navbar --> */}
-
-            {/* <!--Section: Design Block--> */}
-
-            {/* <!--Section: Design Block--> */}
-            </header>
         );
     }
 }
