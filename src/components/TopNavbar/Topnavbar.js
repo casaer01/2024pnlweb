43c6e7fe738f2f3a7bdc1e0572a92099a1fc5639
casaer01 <ecasas182015@gmail.com>
2024-07-22 10:43:04 -0500

    Navbar size changes desktop & mobile

--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "../../images/PNLdistlogo.png";
+import sidebrand from "../../images/papelred.png";
 import './topnavbar.css';
 
 
@@ -7,46 +8,40 @@ class Topnavbar extends Component {
     render() {
         return (
             <div>
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3">
-                <div className="container navBody ">
-                    <img src={logo} alt="Bakery Logo" className="bakeryLogo" />
+                <nav className="navbar navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3">
+                    <div className="container-fluid navBody ">
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                        <img src={sidebrand} alt="paper Picado" className="bakeryLogo" />
+                        <img src={logo} alt="Bakery Logo" className="bakeryLogo" />
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Portfolio</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Reference</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Team</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Contact</a>
-                            </li>
-                        </ul>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#!">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#!">About</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#!">Tortillas</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#!">Orders</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#!">Contact Us</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        
                     </div>
-
-                    
-                    
-                </div>
-            </nav>
+                </nav>
 
             </div>
         );
