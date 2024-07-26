f05d9ddf8f1f72356ea5501a5754bbbecd46c457
casaer01 <ecasas182015@gmail.com>
2024-07-25 20:00:47 -0500

    slight chagnes to navbar

--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -7,31 +7,31 @@ import './topnavbar.css';
 class Topnavbar extends Component {
     render() {
         return (
-            <div>
+            <div className="d-flex">
                 <nav className="navbar d-flex navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3 ">
-                    <div className="container-fluid navBody ">
+                    <div className="container-fluid navBody justify-content-between">
 
-                        <img src={sidebrand} alt="paper Picado" className="bakeryIcon" />
-                        <img src={logo} alt="Bakery Logo" className="bakeryLogo" />
+                        <img src={sidebrand} alt="paper Picado" className="bakeryIcon " />
+                        <img src={logo} alt="Bakery Logo" className="bakeryLogo " />
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                        <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
                         <div className="collapse justify-content-end navbar-collapse" id="navbarSupportedContent">
 
-                            <ul className="navbar-nav rightBlock me-auto mb-2 mb-lg-0">
+                            <ul className="navbar-nav rightBlock mx-auto me-auto mb-2 mb-lg-0">
                                 <li className="nav-item ">
                                     <a className="nav-link " href="#!">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#!">About</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#!">Tortillas</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#!">Orders</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#!">About</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#!">Contact Us</a>
