a96389bf9b2912d8b115c403b0db91b1efb7790d
casaer01 <ecasas182015@gmail.com>
2024-08-19 18:59:16 -0500

    Fixed horizonal scrollbar from About PNL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'clear;
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -7,29 +7,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br></br>
-          This is where everything starts.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <p className="btn btn-primary">Go somewhere</p>
-        </div>
-      </div>
+
     </div>
   );
 }
--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -9,8 +9,8 @@ import './topnavbar.css';
 class Topnavbar extends Component {
     render() {
         return (
-            <div className="d-flex">
-                <nav className="navbar d-flex navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3 ">
+            <div className="d-flex mt-5">
+                <nav className="navbar d-flex navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3">
                     <div className="container-fluid navBody justify-content-between">
 
                         <img src={sidebrand} alt="paper Picado" className="bakeryIcon " />
