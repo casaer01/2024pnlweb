9cbb24a94e76544e55821dd2af3e600f3ca9b97f
casaer01 <ecasas182015@gmail.com>
2024-08-27 14:15:11 -0500

    trying to add black block behind top nav

--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -9,45 +9,48 @@ import './topnavbar.css';
 class Topnavbar extends Component {
     render() {
         return (
-            <div className="d-flex">
-                <nav className="navbar d-flex navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3">
-                    <div className="container-fluid navBody justify-content-between">
+            <div>
+                <div className="blank"></div>
+                <div className="d-flex">
+                    <nav className="navbar d-flex navbar-expand-lg navbar-dark fixed-top mask-custom shadow-3">
+                        <div className="container-fluid navBody justify-content-between">
 
-                        <img src={sidebrand} alt="paper Picado" className="bakeryIcon " />
-                        <img src={logo} alt="Bakery Logo" className="bakeryLogo " />
+                            <img src={sidebrand} alt="paper Picado" className="bakeryIcon " />
+                            <img src={logo} alt="Bakery Logo" className="bakeryLogo " />
 
-                        <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+                            <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
 
-                        <div className="collapse justify-content-end navbar-collapse" id="navbarSupportedContent">
+                            <div className="collapse justify-content-end navbar-collapse" id="navbarSupportedContent">
 
-                            <ul className="navbar-nav rightBlock mx-auto me-auto mb-2 mb-lg-1">
-                                <li className="nav-item ">
-                                    <a className="nav-link" href="/" >Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#!">Tortillas</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#!">Bread</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#!">Orders</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#!">About</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#!">Contact Us</a>
-                                </li>
-                            </ul>
+                                <ul className="navbar-nav rightBlock mx-auto me-auto mb-2 mb-lg-1">
+                                    <li className="nav-item ">
+                                        <a className="nav-link" href="/" >Home</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#!">Tortillas</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#!">Bread</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#!">Orders</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#!">About</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#!">Contact Us</a>
+                                    </li>
+                                </ul>
 
+                            </div>
+                            
                         </div>
-                        
-                    </div>
-                </nav>
+                    </nav>
 
+                </div>
             </div>
         );
     }
