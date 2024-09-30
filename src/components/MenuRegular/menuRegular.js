75eb470427cc3dc0575aa99064a630dffd4b044d
Eros Casas <ecasas182015@gmail.com>
2024-09-30 16:45:24 -0500

    created first card for concha item

--- src/components/MenuRegular/menuRegular.js
+++ src/components/MenuRegular/menuRegular.js
@@ -4,7 +4,56 @@ import './menuRegular.css';
 export class menuRegular extends Component {
   render() {
     return (
-      <div>menuRegular</div>
+      <div className="container">
+
+        <div className='row' >
+          <div className='col-sm'>
+            <img src='' alt='mexican sweet bread concha'></img>
+            <h2>Concha</h2>
+            <p>
+              Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". 
+              The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. 
+              The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.
+            </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+        </div>
+      </div>
     )
   }
 }
