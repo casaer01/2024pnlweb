9f28784f25e916056e1c0ca9ee1f2c41192e92fc
Eros Casas <ecasas182015@gmail.com>
2024-10-09 12:13:34 -0500

    finished first row of breads

--- src/components/MenuRegular/menuRegular.js
+++ src/components/MenuRegular/menuRegular.js
@@ -6,8 +6,8 @@ export class menuRegular extends Component {
     return (
       <div className="container">
 
-        <div className='row' >
-          <div className='col-sm'>
+        <div className="row" >
+          <div className="col-sm">
             <img src='' alt='mexican sweet bread concha'></img>
             <h2>Concha</h2>
             <p>
@@ -23,21 +23,24 @@ export class menuRegular extends Component {
             <img src='' alt='mexican muffin with a concha on top'></img>
             <h2>ManteConcha</h2>
             <p>
-              A mexican mix between the two popular pasteries muffin and a conchas. Instead of a regular muffen top a crisp mini concha takes its place.
+              A mexican mix between the two popular pasteries muffin and a conchas. 
+              Instead of a regular muffen top a crisp mini concha takes its place.
             </p>
           </div>
           <div className="col-sm">
             <img src='' alt='bread desc'></img>
-            <h2>Pastery</h2>
+            <h2>Bolillo</h2>
             <p>
-              desc
+              Simple mexican rolls that can be seen as a mexican's version of the french baguette. 
+              Crispy on the outside and soft on the inside these bolillos are used as a side for soups or can be cut and used for sandwiches.
             </p>
           </div>
           <div className="col-sm">
             <img src='' alt='bread desc'></img>
-            <h2>Pastery</h2>
+            <h2>Telera</h2>
             <p>
-              desc
+              Mexican version of a white bread roll that is soft and fluffy in a oval shape. 
+              Telera can be used to make any range of sandwiches or can simply enjoyed with butter on it.
             </p>
           </div>
         </div>
