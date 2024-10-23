b9fa9095e129a98c66c34df574204cdf8854a6bd
Eros Casas <ecasas182015@gmail.com>
2024-10-22 20:54:22 -0500

    added visual shadows to cards

--- src/components/MenuRegular/menuRegular.js
+++ src/components/MenuRegular/menuRegular.js
@@ -7,27 +7,20 @@ export class menuRegular extends Component {
       <div className="container-fluid menuBackground">
 
         <div className="row" >
-          <div className="col-sm">
+          <div className="card col-sm">
             <img src='' alt='mexican sweet bread concha'></img>
-            <h2>Concha</h2>
-            <p>
-              Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". 
-              The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. 
-              The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.
-            </p>
+            <div className='card-body'>
+              <h2 className='card-title' >Concha</h2>
+              <p className='card-text' >
+                Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". 
+                The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. 
+                The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.
+              </p>
+            </div>
           </div>
         </div>
 
         <div className="row">
-
-          {/* <div className="col-sm">
-            <img src='' alt='mexican muffin with a concha on top'></img>
-            <h2>ManteConcha</h2>
-            <p>
-              A mexican mix between the two popular pasteries muffin and a conchas. 
-              Instead of a regular muffen top a crisp mini concha takes its place.
-            </p>
-          </div> */}
 
           <div class="card col-sm">
             <img src="..." class="card-img-top" alt="mexican muffin with a concha on top"/>
@@ -38,21 +31,26 @@ export class menuRegular extends Component {
             </div>
           </div>
 
-          <div className="col-sm">
-            <img src='' alt='bread desc'></img>
-            <h2>Bolillo</h2>
-            <p>
-              Simple mexican rolls that can be seen as a mexican's version of the french baguette. 
-              Crispy on the outside and soft on the inside these bolillos are used as a side for soups or can be cut and used for sandwiches.
-            </p>
+          <div className="card col-sm">
+            <img src='' className="card-img-top" alt='bread desc'></img>
+            <div className='card-body'>
+              <h3 class="card-title">Bolillo</h3>
+              <p className='card-text' >
+                Simple mexican rolls that can be seen as a mexican's version of the french baguette. 
+                Crispy on the outside and soft on the inside these bolillos are used as a side for soups or can be cut and used for sandwiches.
+              </p>
+            </div>
           </div>
-          <div className="col-sm">
+
+          <div className="card col-sm">
             <img src='' alt='bread desc'></img>
-            <h2>Telera</h2>
-            <p>
-              Mexican version of a white bread roll that is soft and fluffy in a oval shape. 
-              Telera can be used to make any range of sandwiches or can simply enjoyed with butter on it.
-            </p>
+            <div className='card-body'>
+              <h3 className='card-title' >Telera</h3>
+              <p className='card-text' >
+                Mexican version of a white bread roll that is soft and fluffy in a oval shape. 
+                Telera can be used to make any range of sandwiches or can simply enjoyed with butter on it.
+              </p>
+            </div>
           </div>
         </div>
 
