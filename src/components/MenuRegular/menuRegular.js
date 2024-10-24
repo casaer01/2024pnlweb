dee94954b4dadad279ba90431fb9fd3e3ac58fe4
Eros Casas <ecasas182015@gmail.com>
2024-10-23 20:08:12 -0500

    adjusting card layout

--- src/components/MenuRegular/menuRegular.js
+++ src/components/MenuRegular/menuRegular.js
@@ -18,6 +18,19 @@ export class menuRegular extends Component {
               </p>
             </div>
           </div>
+
+          <div className="card col-sm">
+            <img src='' alt='mexican sweet bread concha'></img>
+            <div className='card-body'>
+              <h2 className='card-title' >Concha</h2>
+              <p className='card-text' >
+                Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". 
+                The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. 
+                The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.
+              </p>
+            </div>
+          </div>
+
         </div>
 
         <div className="row">
@@ -29,6 +42,7 @@ export class menuRegular extends Component {
               <p class="card-text">A mexican mix between the two popular pasteries muffin and a conchas. 
               Instead of a regular muffen top a crisp mini concha takes its place.</p>
             </div>
+                      
           </div>
 
           <div className="card col-sm">
