d266d6ab176ee0b8a5c0f1fffb778724b1886104
Eros Casas <ecasas182015@gmail.com>
2025-03-31 19:48:23 -0500

    added Bolillos

--- src/components/MenuRegular/menuRegular.js
+++ src/components/MenuRegular/menuRegular.js
@@ -8,9 +8,7 @@ export class menuRegular extends Component {
       <div className="container-fluid menuBackground">
 
         <div className='row'>
-
           <div className='col-sm cardMain'>
-
             <div className='cardOuter'>
               <div className='cardHead'>
                 <h1>Concha</h1>
@@ -43,47 +41,19 @@ export class menuRegular extends Component {
               </div>
             </div>
           </div>
-
-
-        </div>
-
-        <div className="row" >
-          <div className="card col-sm" style={{ width: '18rem' }}>
-            <img src='' alt='mexican sweet bread concha'></img>
-            <div className='card-body'>
-              <h2 className='card-title' >Concha</h2>
-              <p className='card-text' >
-                Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". 
-                The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. 
-                The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.
-              </p>
-            </div>
-          </div>
-
-          <div className="card col-sm">
-            <img src='' alt='mexican sweet bread concha'></img>
-            <div className='card-body'>
-              <h2 className='card-title' >Concha</h2>
-              <p className='card-text' >
-                Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". 
-                The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. 
-                The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.
-              </p>
-            </div>
-          </div>
-
         </div>
 
         <div className="row">
 
-          <div class="card col-sm">
-            <img src="..." class="card-img-top" alt="mexican muffin with a concha on top"/>
-            <div class="card-body">
-              <h3 class="card-title">ManteConcha</h3>
-              <p class="card-text">A mexican mix between the two popular pasteries muffin and a conchas. 
-              Instead of a regular muffen top a crisp mini concha takes its place.</p>
+          <div className='col-sm cardMain'>
+            <div className='cardOuter'>
+              <div className='cardHead'>
+                <h1>Bolillo</h1>
+              </div>
+              <div className='cardDesc'>
+                <p>Simple mexican rolls that can be seen as a mexican's version of the french baguette. Crispy on the outside and soft on the inside these bolillos are used as a side for soups or can be cut and used for sandwiches.</p>
+              </div>
             </div>
-                      
           </div>
 
           <div className="card col-sm">
