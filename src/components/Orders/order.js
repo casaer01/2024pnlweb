73a78f082a05e557e16cd727e7234d84cd6a0e78
Eros Casas <ecasas182015@gmail.com>
2025-04-24 12:02:10 -0500

    create "order" site

---
+++ src/components/Orders/order.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./order.css";
+
+export class order extends Component{
+    render() {
+        return(
+            <div>
+                order site
+            </div>
+        )
+    }
+}
