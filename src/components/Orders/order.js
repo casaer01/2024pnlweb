bc22b17509e7f2a577483f9cb4108429f991cdea
Eros Casas <ecasas182015@gmail.com>
2025-04-29 17:22:54 -0500

    added more parameter to email

--- src/components/Orders/order.js
+++ src/components/Orders/order.js
@@ -8,8 +8,8 @@ export class order extends Component{
                 <h1>Custom orders</h1>
                 <p>Our bakery takes in custom orders needed by our customers. Do you need a certain amount of conchas, cookies, or slices of cakes, then we can help you fulfill the order for any event.</p>
                 <p>We can be contacted by phone or email</p>
-                <a href="mailto:[email protected]?subject=Feedback&body=Hi there">
-                    Send feedback
+                <a href="mailto:[email protected]?subject=Custom Order&body=Hi there">
+                    
                 </a>
             </div>
         )
