7b5010dcd6c164c10fd7a5fa9e2fa4301e759b68
Eros Casas <ecasas182015@gmail.com>
2025-09-30 10:01:16 -0500

    created cookies Menu

---
+++ src/components/cookiesMenu/cookiesMenu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class cookiesMenu extends Component {
+  render() {
+    return (
+      <div>cookiesMenu</div>
+    )
+  }
+}
+
+export default cookiesMenu
