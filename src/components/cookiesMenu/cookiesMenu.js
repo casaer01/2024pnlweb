38982c429c4b5ffecab5e41a7a272cb87507be0d
Eros Casas <ecasas182015@gmail.com>
2025-10-03 17:29:47 -0500

    added clear format of cookie menu

--- src/components/cookiesMenu/cookiesMenu.js
+++ src/components/cookiesMenu/cookiesMenu.js
@@ -6,38 +6,35 @@ export class cookiesMenu extends Component {
     return (
       <div className="container-fluid menuBackground" >
 
-        <div className='row'>
-          <div className='col-sm cardMain'>
-            <div className='cardOuter'>
-              <div className='cardHead'>
-                <h1>Concha</h1>
-              </div>
-              <div className='cardDesc'>
-                <p>Traditional mexican pan dulce sweet bread or known amoung regulars as a "Concha". The mexican bread called "Concha" is not only one of the sigatures of the bakery but also in mexican culture love for bread. The Concha is a fitting for any time of the day and can be enjoyed with milk, hot chocolate, or even with refried beans.</p>
-              </div>
+        <div className="row">
+
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
             </div>
           </div>
 
-          <div className='col-sm cardMain'>
-            <div className='cardOuter'>
-              <div className='cardHead'>
-                <img src={cookie} className='imgCard' alt='bread' />
-                <h1>Concha Trigo</h1>
-              </div>
-              <div className='cardDesc'>
-                <p>Traditional mexican pan dulce that uses wheat flour instead of white flour. A bread with more fiber and vitamins which is perfect for those wanting a healther option.</p>
-              </div>
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
             </div>
           </div>
 
-          <div className='col-sm cardMain'>
-            <div className='cardOuter'>
-              <div className='cardHead'>
-                <h1>ManteConcha</h1>
-              </div>
-              <div className='cardDesc'>
-                <p>A mexican mix between cupcakes and mexican conchas. Instead of a regular muffen top a small concha is placed.</p>
-              </div>
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
             </div>
           </div>
         </div>
