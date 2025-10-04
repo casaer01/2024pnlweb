cea15f7ac884f0e592b7ceb95218a8b24579f9f3
Eros Casas <ecasas182015@gmail.com>
2025-10-03 19:02:20 -0500

    added first row of cookies.

--- src/components/cookiesMenu/cookiesMenu.js
+++ src/components/cookiesMenu/cookiesMenu.js
@@ -9,14 +9,39 @@ export class cookiesMenu extends Component {
         <div className="row">
 
           <div className="card col-sm">
-            <img src='' alt=''></img>
+            <img src='' alt='Cookie topped with sphere shaped sprinkles.'></img>
             <div className='card-body'>
-              <h3 className='card-title' ></h3>
+              <h3 className='card-title' >Granjea</h3>
               <p className='card-text' >
-
+                Traditional mexican cookies that is covered in ball shaped sprinkles called grageas. The cookie itself taste mildly sweet with the grageas giving it a crunchy outside.
               </p>
             </div>
           </div>
+
+          <div className="card col-sm">
+            <img src='' alt='Swirl shaped butter tasted cookie'></img>
+            <div className='card-body'>
+              <h3 className='card-title' >Mantequilla</h3>
+              <p className='card-text' >
+                A simple mexican butter cookie. Rich and creamy with a smooth texture that melts in your mouth.
+              </p>
+            </div>
+          </div>
+
+          <div className="card col-sm">
+            <img src='' alt='Classic M&M sprinkles onto mexican cookie'></img>
+            <div className='card-body'>
+              <h3 className='card-title' >M&M</h3>
+              <p className='card-text' >
+                golden-brown cookie with a soft, chewy center which is coated with vibrant and colorful M&M chocolates.
+              </p>
+            </div>
+          </div>
+          
+        </div>
+
+
+        <div className="row">
 
           <div className="card col-sm">
             <img src='' alt=''></img>
@@ -37,6 +62,52 @@ export class cookiesMenu extends Component {
               </p>
             </div>
           </div>
+
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className="row">
+
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
+            </div>
+          </div>
+
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
+            </div>
+          </div>
+
+          <div className="card col-sm">
+            <img src='' alt=''></img>
+            <div className='card-body'>
+              <h3 className='card-title' ></h3>
+              <p className='card-text' >
+
+              </p>
+            </div>
+          </div>
+
         </div>
 
       </div>
