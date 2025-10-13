d16a0902462bb094b74514a9cf8e0e408346362f
Eros Casas <ecasas182015@gmail.com>
2025-10-13 16:14:02 -0500

    following new method for router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg'clear;
 import './App.css';
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, BrowserRouter } from 'react-router-dom';
 
 import Topnavbar from './components/TopNavbar/Topnavbar';
 import InfoBar from './components/InfoBar/infoBar';
@@ -15,29 +15,34 @@ import { Component } from 'react';
 import { render } from '@testing-library/react';
 
 
-// function App() {
-//   return (
-//     <div className="App">
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={Topnavbar} />
 
-//     </div>
-//   );
-// }
+
+        <Route path='' Component={Footer} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 // https://www.w3schools.com/react/react_router.asp current version of react router.
 
-class App extends Component {
-  render() {
-    return (
-      <Router basename="/app">
-        <Route>
-          <Route path="/" Component={Topnavbar} />
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router basename="/app">
+//         <Route>
+//           <Route path="/" Component={Topnavbar} />
 
 
-          <Route path='' Component={Footer} />
-        </Route>
-      </Router>
-    );
-  }
-}
+//           <Route path='' Component={Footer} />
+//         </Route>
+//       </Router>
+//     );
+//   }
+// }
 
 export default App;
