7ddbac3f7e0201c62c2809ead4d8b560f89ca178
Eros Casas <ecasas182015@gmail.com>
2025-10-17 15:09:59 -0500

    only top nav bar is showing up.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg'clear;
 import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { createRoot } from "react-dom/client";
+import { Routes, Route } from 'react-router-dom';
+// import { createRoot } from "react-dom/client";
 
 import Topnavbar from './components/TopNavbar/Topnavbar';
 import InfoBar from './components/InfoBar/infoBar';
@@ -10,11 +10,10 @@ import Footer from './components/Footer/footer';
 import AboutUs from './components/AboutPNL/AboutPNL';
 import MenuRegular from './components/MenuRegular/menuRegular';
 import Order from './components/Orders/order';
-import cookiesMenu from './components/cookiesMenu/cookiesMenu'
-import { Component } from 'react';
-import { render } from '@testing-library/react';
+import CookiesMenu from './components/cookiesMenu/cookiesMenu'
 
-const root = createRoot(document.getElementById("root"));
+
+// const root = createRoot(document.getElementById("root"));
 
 export default function App() {
   return (
@@ -26,6 +25,12 @@ export default function App() {
           <Route path='/' element={ <InfoBar/> } />
 
           <Route path='/aboutus' element={ <AboutUs/> } />
+
+          <Route path='/' element={<MenuRegular/>} />
+
+          <Route path='/' element={ <Order/>} />
+
+          <Route path='/' element={ < CookiesMenu />} />
 
           <Route path='/' element={ <Footer/> } />
         </Routes>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createRoot } from "react-dom/client";
@@ -12,14 +12,14 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 // Bootstrap Icons
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Topnavbar from './components/TopNavbar/Topnavbar';
-import InfoBar from './components/InfoBar/infoBar';
-import SlideShow from './components/SlideShow/Slideshow';
-import Footer from './components/Footer/footer';
-import AboutUs from './components/AboutPNL/AboutPNL';
-import MenuRegular from './components/MenuRegular/menuRegular';
-import Order from './components/Orders/order';
-import cookiesMenu from './components/cookiesMenu/cookiesMenu'
+// import Topnavbar from './components/TopNavbar/Topnavbar';
+// import InfoBar from './components/InfoBar/infoBar';
+// import SlideShow from './components/SlideShow/Slideshow';
+// import Footer from './components/Footer/footer';
+// import AboutUs from './components/AboutPNL/AboutPNL';
+// import MenuRegular from './components/MenuRegular/menuRegular';
+// import Order from './components/Orders/order';
+// import cookiesMenu from './components/cookiesMenu/cookiesMenu'
 
 // import reportWebVitals from './reportWebVitals';
 // import 'bootstrap/dist/css/bootstrap.css';
