9af0131502a0adabcf9246e1723c442b1c612dc0
Eros Casas <ecasas182015@gmail.com>
2025-11-03 18:10:48 -0600

    offical document underdevelop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg'clear;
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Layout from './Layout';
 // import { createRoot } from "react-dom/client";
 
@@ -28,7 +28,7 @@ function App() {
           {/* <Route path='/' element={ <InfoBar/> } />           */}
 
           <Route path='/' element={ <SlideShow/> } > 
-            <Route index element={ <AboutUs /> } />
+            <Route index element={ <AboutUs/> }  />
           </Route>
 
 
