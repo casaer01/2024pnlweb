640a10297f92963c06900231d1c53f8c63f6ee50
Eros Casas <ecasas182015@gmail.com>
2025-11-17 13:24:06 -0600

    comments about offical document

--- src/App.js
+++ src/App.js
@@ -42,10 +42,8 @@ function App() {
   );
 }
 
-// Use useRoutes to display page with mulitple elements.  https://stackoverflow.com/questions/40541994/multiple-path-names-for-a-same-component-in-react-router
-// Use nested routes?
-
 // Layout routes may be the true answer https://reactrouter.com/6.30.1/route/route#layout-routes
+//Offical document not enough & explains too little.
 
 // https://www.w3schools.com/react/react_router.asp current version of react router.
 
