dd41b55e1663cdd5d74d631f548610bc9f75d8d2
Eros Casas <ecasas182015@gmail.com>
2025-12-02 16:39:04 -0600

    combine component named as Homepage.js

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import SlideShow from './components/SlideShow/Slideshow';
+import Aboutus from './components/AboutPNL/AboutPNL';
 
 export default class Homepage extends Component {
-  render() {
-    return (
-      <div>Homepage</div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <SlideShow />
+                <Aboutus />
+            </div>
+        )
+    }
 }
