970aa97d718a7e38b388fa9ec9a44b435c8d5ba9
Eros Casas <ecasas182015@gmail.com>
2025-12-17 12:46:43 -0600

    started tortillas layout

--- src/components/TortillasMenu/TortillasMenu.js
+++ src/components/TortillasMenu/TortillasMenu.js
@@ -4,7 +4,44 @@ import './TortillasMenu.css';
 export default class TortillasMenu extends Component {
     render() {
         return (
-            <div>TortillasMenu</div>
+        <div className="container-fluid menuBackground">
+            <div className='row'>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1></h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1></h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1></h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
         )
     }
 }
