a7d6811370af7e013111daa35042e11e30dc90a8
Eros Casas <ecasas182015@gmail.com>
2025-12-17 14:30:19 -0600

    added names of Tortillas

--- src/components/TortillasMenu/TortillasMenu.js
+++ src/components/TortillasMenu/TortillasMenu.js
@@ -10,7 +10,18 @@ export default class TortillasMenu extends Component {
                 <div className='col-sm cardMain'>
                     <div className='cardOuter'>
                     <div className='cardHead'>
-                        <h1></h1>
+                        <h1>Traditional</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p>desc</p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Trigo</h1>
                     </div>
                     <div className='cardDesc'>
                         <p></p>
@@ -21,18 +32,7 @@ export default class TortillasMenu extends Component {
                 <div className='col-sm cardMain'>
                     <div className='cardOuter'>
                     <div className='cardHead'>
-                        <h1></h1>
-                    </div>
-                    <div className='cardDesc'>
-                        <p></p>
-                    </div>
-                    </div>
-                </div>
-
-                <div className='col-sm cardMain'>
-                    <div className='cardOuter'>
-                    <div className='cardHead'>
-                        <h1></h1>
+                        <h1>Margarine</h1>
                     </div>
                     <div className='cardDesc'>
                         <p></p>
@@ -41,6 +41,120 @@ export default class TortillasMenu extends Component {
                 </div>
 
             </div>
+
+            <div className='row'>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Chile de Arbol</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Nopal</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Habanero</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='row'>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Chile Guajillo</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Jalapeno</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Chipotle</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='row'>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Manteca Inca</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Mole</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='col-sm cardMain'>
+                    <div className='cardOuter'>
+                    <div className='cardHead'>
+                        <h1>Spinach</h1>
+                    </div>
+                    <div className='cardDesc'>
+                        <p></p>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+            
+
         </div>
         )
     }
